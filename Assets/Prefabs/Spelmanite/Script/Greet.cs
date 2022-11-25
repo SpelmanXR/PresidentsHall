ab40db234a9062f0f938b48090b15596c5bda8b6
jvolcy <jerry.volcy@gmail.com>
2022-11-25 06:21:38 -0500

    Added greeting animation

---
+++ Assets/Prefabs/Spelmanite/Script/Greet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greet : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        animator.SetTrigger("talk");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        animator.SetTrigger("idle");
+    }
+}
