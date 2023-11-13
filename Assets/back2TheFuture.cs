ae95b7a292d46d6c83cd22f364fdc0c48c2aa286
DevynW <116035427+DevynW@users.noreply.github.com>
2023-11-13 16:04:58 -0500

    Many Fixings!!!!!
    Fixed the portal clicking issue
    Fixed the teleportation issues
    Fixed portal popup issues
    Fixed the whole teleport script
    Also fixed the waterline

--- Assets/back2TheFuture.cs
+++ Assets/back2TheFuture.cs
@@ -12,7 +12,7 @@ public class back2TheFuture : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SampleSceneobj = "SampleScene";
+        SampleSceneobj = "ExhibitionHall";
     }
 
     // Update is called once per frame
--- Assets/telepot2campus.cs
+++ Assets/telepot2campus.cs
@@ -11,26 +11,54 @@ public class telepot2campus : MonoBehaviour
     public back2TheFuture BTF;
     public Vector3 PositionInScene = new Vector3(0f, 0f, -5f);
     public Vector3 AngleInScene = new Vector3(0f, -40f, 0f);
+    static GameObject rabbitHole = null;
+    GameObject Terrain;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (rabbitHole == null)
+        {
+            rabbitHole = GameObject.FindGameObjectWithTag("rabbitHole");
+            if (rabbitHole == null)
+            {
+                Debug.Log("telepot2campus.cs: I can't find the rabbit hole!!!");
+
+            }
+            else
+            {
+                Debug.Log(campusSceneName + " found the rabbit hole");
+            }
+            rabbitHole.SetActive(false);
+        }
+
+        Terrain = GameObject.FindGameObjectWithTag("Terrain");
+            if(Terrain == null)
+        {
+            Debug.Log(campusSceneName + " can't find the terrain!!!!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void teleport()
     {
+        
         BTF.ogPosition = XRObject.transform.position;
         BTF.ogOrientation = XRObject.transform.localEulerAngles;
         XRObject.transform.position = PositionInScene;
         XRObject.transform.localEulerAngles = AngleInScene;
-        SceneManager.LoadSceneAsync(campusSceneName,LoadSceneMode.Additive);
+        Debug.Log("switching to " + campusSceneName);
+        SceneManager.LoadSceneAsync(campusSceneName, LoadSceneMode.Additive);
         RenderSettings.skybox = sceneBoxName;
+        Debug.Log("switched to " + campusSceneName);
+        rabbitHole.SetActive(true);
+        Terrain.SetActive(false);
     }
 }
